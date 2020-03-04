5b7abefec7956278fb8d18f27476dd9569c20338
Jaspinder Sabharwal <jas@jaspinder.net>
2020-03-04 04:40:02 +0000

    eqArray uploaded

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  let trueOrFalse;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      trueOrFalse = false;
+    }
+    trueOrFalse = true;
+  }
+  return trueOrFalse;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
