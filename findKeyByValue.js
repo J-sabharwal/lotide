9182dabdf06761d66b4ebd3a5792a3d0e3908b2b
Jaspinder Sabharwal <jas@jaspinder.net>
2020-03-05 04:10:55 +0000

    added findKeybyValue

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
+  }
+};
+
+const findKeyByValue = function(showGenre, show) {
+  // Search through each key-value
+  // If value is match the argument
+  // return values key
+ 
+  for (let key in showGenre) {
+    if (showGenre[key] === show) {
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
