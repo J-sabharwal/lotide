1bde78db56fa5c4ba0f85f9431cc9a505b811936
Jaspinder Sabharwal <jas@jaspinder.net>
2020-03-05 03:07:12 +0000

    pushed letterPosition.js

---
+++ letterPositions.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  
+  let i = 0;
+  for (let letter of sentence) {
+   
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter].push(i);
+      } else {
+        results[letter] = [i];
+      }
+    }
+    i++;
+  }
+
+  return results;
+};
+
+// Check the string index against a index number, using the character you want to check
+assertEqual(letterPositions("hello").h, [0]);
