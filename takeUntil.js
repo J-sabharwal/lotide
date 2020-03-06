9a4f2ffea1a05b60abe1a7de246d15bc73d7f8f9
Jaspinder Sabharwal <jas@jaspinder.net>
2020-03-06 00:37:08 +0000

    added takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,42 @@
+const assertEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Function created to take in array and callback as parameters
+// loop through array and evaluate if condition is false to push element to array otherwise stop loop with break.
+let takeUntil = function(array, callback) {
+  let newArr = [];
+  for (let element of array) {
+    if (callback(element) === false) {
+      newArr.push(element);
+    } else {
+      break;
+    }
+  }
+  return newArr;
+};
+
+// New Array
+// Created a variable and called takeUntil function passing in array and callback function as argument
+const words = ["ground", "control", "to", "major", "tom"];
+const results = takeUntil(words, elem => elem === "major");
+assertEqual(results, ["ground", "control", "to"]);
+
+console.log("---");
+
+const data = [2, 4, 6, 23, 5, 54, 6];
+const results2 = takeUntil(data, elem => elem > 20);
+assertEqual(results2, [2, "4", 6]);
