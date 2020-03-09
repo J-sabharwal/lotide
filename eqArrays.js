01c6402aae59b250e4f4594969e878e91e11f24b
Jaspinder Sabharwal <jas@jaspinder.net>
2020-03-09 19:54:46 +0000

    refactored eqArrays with @zeipar

--- eqArrays.js
+++ eqArrays.js
@@ -1,24 +1,19 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${[actual]} === ${[expected]}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅ Assertion Passed: ${[actual]} === ${[expected]}`);
+//   } else {
+//     console.log(`🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
+//   }
+// };
 
 const eqArrays = function(array1, array2) {
-  let trueOrFalse;
+  
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      trueOrFalse = false;
+      return false;
     }
-    trueOrFalse = true;
   }
-  return trueOrFalse;
+  return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
